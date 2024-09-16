b2f5e97fb4b15690b09cf15a95e82f2d5cbb2023
stash <stashomster@gmail.com>
2024-09-17 00:21:08 +0300

    test

--- price.py
+++ price.py
@@ -1,2 +1,9 @@
 class Price:
-    pass
+    def __init__(self, c=0, e=0, b=0, t=0):
+        if all([0 <= c <= 5, 0 <= e <= 5, 0 <= b <= 5, 0 <= t <= 5]):
+            self.c = c
+            self.e = e
+            self.b = b
+            self.t = t
+        else:
+            raise ValueError
