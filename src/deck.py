85fe4b4f8837a4ebfa16ddaeb84b53e4a2e287ef
stash <stashomster@gmail.com>
2024-11-11 02:56:45 +0300

    Заново переписан класс Deck

--- src/deck.py
+++ src/deck.py
@@ -1,35 +1,39 @@
 import random
-import typing
-
 from src.card import Card
 
 class Deck:
-    def __init__(self, cards: None | list[Card]):
+    def __init__(self, cards=None):
         if cards is None:
             cards = Card.all_cards()
             random.shuffle(cards)
-        self.cards: list[Card] = cards
-
-    def __repr__(self):
-        return self.save()
+        self.cards = cards  # список карт
 
     def __eq__(self, other):
         if isinstance(other, str):
             other = Deck.load(other)
         return self.cards == other.cards
 
-    def save(self) -> str:
-        scards = [c.save() for c in self.cards]
-        s = ' '.join(scards)
+    def __repr__(self):
+        return self.save()
+
+    def save(self):
+        save_cards = []
+        for c in self.cards:
+            save_cards.append(c.save())
+        s = ' '.join(save_cards)
         return s
 
     @classmethod
-    def load(cls, text: str) -> typing.Self:
-        cards = [Card.load(s) for s in text.split()]
-        return cls(cards=cards)
+    def load(cls, text):
+        cards = []
+        for s in text.split():
+            cards.append(Card.load(s))
+        return cls(cards)
 
     def draw_card(self):
+        if len(self.cards) == 0:  # Проверяем, есть ли карты в колоде
+            return None
         return self.cards.pop()
 
     def shuffle(self):
-        random.shuffle(self.cards)
+        random.shuffle(self.cards)  # Перемешиваем карты
