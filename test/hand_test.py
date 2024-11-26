76c416900ae35de962a1b91bb0e6bfa3b9b82203
stash <stashomster@gmail.com>
2024-11-26 13:23:10 +0300

    Написано 2 теста для hand

---
+++ test/hand_test.py
@@ -0,0 +1,17 @@
+import pytest
+from src.card import Card
+from src.hand import Hand
+
+cards = [Card.load('ТМК'), Card.load('КБЗ'), Card.load('ЗЗЗ')]
+
+
+def test_init():
+    i = Hand(cards)
+    assert i.card_list == cards
+
+
+def test_save():
+    s = Hand(cards=cards)
+    assert s.save_cards() == 'ТМК КБЗ ЗЗЗ'
+
+
