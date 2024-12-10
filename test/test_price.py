1f9e4c844ab1b5a4bad925f28885b0f864602f03
stash <stashomster@gmail.com>
2024-12-10 14:56:11 +0300

    Написано два теста: validation и load, нерабочий test_add

--- test/test_price.py
+++ test/test_price.py
@@ -16,6 +16,16 @@ def test_save():
     p = Price(Т=1, М=2, К=3, Б=4, З=5)
     assert repr(p) == 'Т:1 М:2 К:3 Б:4 З:5'
 
+def test_validation():
+    with pytest.raises(ValueError):
+        p = Price(Т=1, М=2, К=3, Б=4, З=6)
 
 
+def test_load():
+    t = "Т:1 М:2 К:3 Б:4 З:5"
+    assert Price.load(t) == Price(Т=1, М=2, К=3, Б=4, З=5)
 
+'''def test_add():
+    p = Price(Т=2, М=2, К=3, Б=4, З=5)
+    p.add("БЗЗ")
+    assert p.t == p.m == 2'''
