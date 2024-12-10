089c3368230d44b625e8d779d642bd0ba027d60c
stash <stashomster@gmail.com>
2024-12-10 16:28:17 +0300

    Добавлены тесты: test_score, нерабочие: test_add_card, test_load

--- test/hand_test.py
+++ test/hand_test.py
@@ -1,7 +1,7 @@
 import pytest
 from src.card import Card
 from src.hand import Hand
-
+from src.hand import Price
 cards = [Card.load('ТМК'), Card.load('КБЗ'), Card.load('ЗЗЗ')]
 
 
@@ -14,5 +14,17 @@ def test_save():
     s = Hand(cards=cards)
     assert s.save_cards() == 'ТМК КБЗ ЗЗЗ'
 
+'''def test_load():
+    s = Hand.load_cards('ТМК КБЗ ЗЗЗ')
+    assert s == Hand(cards)'''
 
 
+'''def test_add_card():
+    h = Hand.load_cards('ТМК КБЗ ЗЗЗ')
+    h.add_card(Card.load('ККК'))
+    assert repr(h) == 'ТМК КБЗ ЗЗЗ ККК'
+    '''
+def test_score():
+    h = Hand(cards=[Card.load('МКЗ')])
+    p = Price(Т=1, М=2, К=3, Б=4, З=5)
+    assert h.score(p) == 2 + 3 + 5
