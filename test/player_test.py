a716b04bf7ef6e38b11011587f001d81d19e4692
stash <stashomster@gmail.com>
2024-12-24 15:03:34 +0300

    Добавлены нерабочие тесты: test_init, test_load, test_save

---
+++ test/player_test.py
@@ -0,0 +1,26 @@
+from src.hand import Hand
+from src.player import Player
+from src.card import Card
+
+
+cards = [Card.load('ТМК'), Card.load('КБЗ'), Card.load('ЗЗЗ')] #[ТМК, КБЗ, ЗЗЗ]
+
+def test_init():
+    h = Hand.load_cards('ТМК КБЗ ЗЗЗ')
+    p = Player(n='Bob', h=h)
+    assert p.name == 'Bob'
+    assert p.hand == h
+
+
+def test_load():
+    d = {'n': 'Bob', 'h': 'ТМК КБЗ ЗЗЗ'}
+    h_exp = Hand.load_cards('ТМК КБЗ ЗЗЗ')
+    p_exp = Player(n='Leo', h=h_exp)
+    assert str(p_exp) == str(Player.load(d))
+
+
+def test_save():
+    h = Hand.load_cards('ТМК КБЗ ЗЗЗ')
+    p = Player(n='Bob', h=h)
+    assert str(p) == 'Bob: ТТМК КБЗ ЗЗЗ'
+    assert p.save() == {'n': 'Bob', 'h': 'ТМК КБЗ ЗЗЗ'}
