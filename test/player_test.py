3a4f6ad573bf0a70b2f292ca7ade2c5cc48d02bf
stash <stashomster@gmail.com>
2024-12-24 17:23:15 +0300

    2/3 функции начали работать

--- test/player_test.py
+++ test/player_test.py
@@ -3,7 +3,7 @@ from src.player import Player
 from src.card import Card
 
 
-cards = [Card.load('ТМК'), Card.load('КБЗ'), Card.load('ЗЗЗ')] #[ТМК, КБЗ, ЗЗЗ]
+cards = [Card.load(' '), Card.load('КБЗ'), Card.load('ЗЗЗ')] #[ТМК, КБЗ, ЗЗЗ]
 
 def test_init():
     h = Hand.load('ТМК КБЗ ЗЗЗ')
@@ -13,14 +13,14 @@ def test_init():
 
 
 def test_load():
-    d = {'n': 'Bob', 'h': 'ТМК КБЗ ЗЗЗ'}
+    d = {'name': 'Bob', 'hand': 'ТМК КБЗ ЗЗЗ'}
     h_exp = Hand.load('ТМК КБЗ ЗЗЗ')
-    p_exp = Player(n='Leo', h=h_exp)
+    p_exp = Player(n='Bob', h=h_exp)
     assert str(p_exp) == str(Player.load(d))
 
 
 def test_save():
     h = Hand.load('ТМК КБЗ ЗЗЗ')
     p = Player(n='Bob', h=h)
-    assert str(p) == 'Bob: ТТМК КБЗ ЗЗЗ'
-    assert p.save() == {'n': 'Bob', 'h': 'ТМК КБЗ ЗЗЗ'}
+    assert str(p) == 'Bob: ТМК КБЗ ЗЗЗ'
+    assert p.save() == {'name': 'Bob', 'hand': 'ТМК КБЗ ЗЗЗ'}
